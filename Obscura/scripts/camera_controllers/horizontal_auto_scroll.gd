2f8db002e5e412805bdc26faf0877a451d36a846
puncakedev <andrewtran200@gmail.com>
2024-10-30 23:02:36 -0700

    Stage 2

--- Obscura/scripts/camera_controllers/horizontal_auto_scroll.gd
+++ Obscura/scripts/camera_controllers/horizontal_auto_scroll.gd
@@ -1,6 +1,9 @@
 class_name HorizontalAutoScroll
 extends CameraControllerBase
 
+@export var top_left := Vector2(-10, 10)
+@export var bottom_right := Vector2(10, -10)
+@export var autoscroll_speed := Vector3(5, 0, 5)
 
 func _ready() -> void:
 	super()
@@ -14,7 +17,30 @@ func _process(delta: float) -> void:
 	if draw_camera_logic:
 		draw_logic()
 	
-	position = target.position
+	var tpos = target.global_position
+	var cpos = global_position
+	
+	#Vessel and camera autoscroll
+	var cam_autoscroll: Vector3 = autoscroll_speed * delta
+	global_position += cam_autoscroll
+	target.global_position += cam_autoscroll
+	
+	#Setting boundaries and vessel push
+	var left_bound = (tpos.x - target.WIDTH / 2.0) - (cpos.x + top_left.x)
+	if left_bound < 0:
+		target.global_position.x -= left_bound
+		
+	var top_bound = (tpos.z + target.HEIGHT / 2.0) - (cpos.z + top_left.y)
+	if top_bound > 0:
+		target.global_position.z -= top_bound
+		
+	var right_bound = (tpos.x + target.WIDTH / 2.0) - (cpos.x + bottom_right.x)
+	if right_bound > 0:
+		target.global_position.x -= right_bound
+	
+	var bot_bound = (tpos.z - target.WIDTH / 2.0) - (cpos.z + bottom_right.y)
+	if bot_bound < 0:
+		target.global_position.z -= bot_bound
 	
 	super(delta)
 
@@ -27,12 +53,23 @@ func draw_logic() -> void:
 	mesh_instance.mesh = immediate_mesh
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
 	
-	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	immediate_mesh.surface_add_vertex(Vector3(0, 0, 5))
-	immediate_mesh.surface_add_vertex(Vector3(0, 0, -5))
+	var left: float = top_left.x
+	var top: float = top_left.y
+	var right: float = bottom_right.x
+	var bottom: float = bottom_right.y
 	
-	immediate_mesh.surface_add_vertex(Vector3(5, 0, 0))
-	immediate_mesh.surface_add_vertex(Vector3(-5, 0, 0))
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
 	immediate_mesh.surface_end()
 
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
---
+++ Obscura/scripts/camera_controllers/position_lock_and_lerp.gd
@@ -0,0 +1,14 @@
+class_name PositionLockAndLerp
+extends CameraControllerBase
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func draw_logic() -> void:
+	pass
